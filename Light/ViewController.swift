38d95d038c26c8b648bfa383a912970cb035bc49
Rich Castagna <idev.tru@gmail.com>
2018-11-16 11:38:36 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Richard Castagna on 11/9/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
